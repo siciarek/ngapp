c491aea361236fc18eb64c741554070996fe3e7e
Jacek Siciarek <jacek.siciarek@escore.pl>
2016-04-13 09:29:27 +0200

    Fix code due to jshint validation

--- app/shared/menu/menuDirective.js
+++ app/shared/menu/menuDirective.js
@@ -1,7 +1,13 @@
-angular.module('app').directive('menu', function () {
-    return {
-        templateUrl: '/app/shared/menu/menuView.html',
-        restrict: 'E',
-        replace: true
-    };
-});
+(function () {
+    'use strict';
+
+    angular.module('app').directive('menu', menu);
+
+    function menu() {
+        return {
+            templateUrl: '/app/shared/menu/menuView.html',
+            restrict: 'E',
+            replace: true
+        };
+    }
+})();
